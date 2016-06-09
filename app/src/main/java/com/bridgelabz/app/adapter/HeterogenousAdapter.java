4c3ee707f66dcee31c89f344de29250db221dcec
chhota89 <chhota89@gmail.com>
2016-06-09 13:54:34 +0530

    Hetrogenous remaning file added

---
+++ app/src/main/java/com/bridgelabz/app/adapter/HeterogenousAdapter.java
@@ -0,0 +1,94 @@
+package com.bridgelabz.app.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bridgelabz.app.R;
+import com.bridgelabz.app.model.UserInfo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bridgeit on 9/6/16.
+ */
+
+public class HeterogenousAdapter extends RecyclerView.Adapter<HeterogenousAdapter.MyViewHolder>{
+
+    ArrayList<Object> list;
+    private  final int  IMAGE_TYPE=0,USER_INFO_TYPE=1;
+    public  HeterogenousAdapter(ArrayList<Object> list){
+        this.list=list;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class UserInfoViewHolder extends  MyViewHolder{
+        public TextView firstName,LastName;
+        public UserInfoViewHolder(View itemView) {
+            super(itemView);
+            firstName=(TextView)itemView.findViewById(R.id.firstName);
+            LastName=(TextView)itemView.findViewById(R.id.lastName);
+        }
+    }
+
+    public class ImageViewHolder extends  MyViewHolder{
+        public ImageView imageView;
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            imageView=(ImageView)itemView.findViewById(R.id.imageView);
+        }
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        MyViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if(viewType ==USER_INFO_TYPE){
+            View viewUserInfo=inflater.inflate(R.layout.adapter_details_layout,parent,false);
+            viewHolder=new UserInfoViewHolder(viewUserInfo);
+        }else {
+            View viewUserInfo=inflater.inflate(R.layout.adapter_image_layout,parent,false);
+            viewHolder=new ImageViewHolder(viewUserInfo);
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        if(holder.getItemViewType()== USER_INFO_TYPE){
+            UserInfoViewHolder userInfoViewHolder=(UserInfoViewHolder)holder;
+            UserInfo userInfo=(UserInfo)list.get(position);
+            userInfoViewHolder.firstName.setText(userInfo.getFirstName());
+            userInfoViewHolder.LastName.setText(userInfo.getLastName());
+        }else {
+            ImageViewHolder imageViewHolder=(ImageViewHolder)holder;
+            String imageName=(String) list.get(position);
+
+            ///imageViewHolder.imageView.setImageDrawable;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(list.get(position) instanceof UserInfo)
+            return USER_INFO_TYPE;
+        else if(list.get(position) instanceof String)
+            return  IMAGE_TYPE;
+        else
+            return -1;
+    }
+}
