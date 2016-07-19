35857af8ebe07ff2aeb2220d9f74700275afca6f
ESHVAR MALI <eshvar89@hotmail.com>
2016-07-19 10:51:35 +0530

    Added the activity  android_material_design and volley_feed into the AndroidManitest.xml.

--- app/src/main/java/com/bridgelabz/app/activity/ConstraintExample.java
+++ app/src/main/java/com/bridgelabz/app/activity/ConstraintExample.java
@@ -2,6 +2,11 @@ package com.bridgelabz.app.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bridgelabz.app.R;
 
@@ -10,11 +15,40 @@ import com.bridgelabz.app.R;
  */
 
 public class ConstraintExample extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.practice);
 
+        editText= (EditText) findViewById(R.id.editText);
+       /* editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (s == EditorInfo.IME_ACTION_SEND){
+                    Toast.makeText(ConstraintExample.this,"Send button clicked",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });*/
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    Toast.makeText(ConstraintExample.this,"Send button clicked",Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/bridgelabz/app/database/RealmController.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.app.database;
-
-/**
- * Created by bridgeit on 18/7/16.
- */
-
-public class RealmController implements IRealmRepository{
-}
